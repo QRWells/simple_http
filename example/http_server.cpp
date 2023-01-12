d0c31dde702e36ba5c8f8c8b1acdb2733b1315b7
QRWells <qirui.wang@moegi.waseda.jp>
2023-01-13 02:39:58 +0900

    feat(example): add a example for http server

---
+++ example/http_server.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file http_server.cpp
+ * @author Qirui Wang (qirui.wang@moegi.waseda.jp)
+ * @brief
+ * @version 0.1
+ * @date 2023-01-12
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+
+#include <net/http/http_server.hpp>
+
+using namespace std;
+using namespace simple_http::net;
+using namespace simple_http::net::http;
+
+int main(int argc, char* argv[]) {
+  EventLoopThread thread;
+  thread.Run();
+
+  InetAddr addr{8888};
+
+  http::HttpServer server{thread.GetLoop(), {8888}};
+
+  server.Get("/hello", [](HttpRequest const& req, HttpResponse& resp) {
+    resp.SetStatusCode(200);
+    resp.SetStatusMessage("OK");
+    resp.SetBody("Hello World!");
+  });
+
+  server.Start();
+
+  thread.Wait();
+  return 0;
+}
--- example/xmake.lua
+++ example/xmake.lua
@@ -7,3 +7,13 @@ target("example_tcp_server")
   add_files("tcp_server.cpp")
 
   add_syslinks("pthread")
+
+target("example_http_server")
+  set_default(true)
+  set_kind("binary")
+
+  add_deps("static_lib")
+
+  add_files("http_server.cpp")
+
+  add_syslinks("pthread")
